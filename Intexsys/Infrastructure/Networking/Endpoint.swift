1bb2b236f6ba1e44a43614ca6a8068dcb331b1bb
Jans Pavlovs <jans.pavlovs@gmail.com>
2021-01-24 18:22:28 +0200

    Extract version from strings

--- Intexsys/Infrastructure/Networking/Endpoint.swift
+++ Intexsys/Infrastructure/Networking/Endpoint.swift
@@ -17,12 +17,16 @@ enum Endpoint {
 // MARK: -
 
 extension Endpoint {
+    var version: String {
+        "0.5"
+    }
+
     func path() -> String {
         switch self {
         case .categories:
-            return "/api/0.5/categories"
+            return "/api/\(version)/categories"
         case .products(let category):
-            return "/iv-api/0.5/catalog/\(category.url)/products?_iv_include=gridProducts"
+            return "/iv-api/\(version)/catalog/\(category.url)/products?_iv_include=gridProducts"
         }
     }
 }
