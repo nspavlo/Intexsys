4b9a31a04a2d4c97d50c2a456fce3bf98fd8a847
Jans Pavlovs <jans.pavlovs@gmail.com>
2021-01-24 15:57:56 +0200

    Add products flow coordinator

---
+++ Intexsys/Presentation/Products/Flows/ProductsFlowCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsFlowCoordinator.swift
+//  Intexsys
+//
+//  Created by Jans Pavlovs on 24/01/2021.
+//
+
+import Foundation
+
+// MARK: Initialization
+
+final class ProductsFlowCoordinator {}
+
+// MARK: Coordinator
+
+extension ProductsFlowCoordinator: Coordinator {
+    func start() {}
+}
