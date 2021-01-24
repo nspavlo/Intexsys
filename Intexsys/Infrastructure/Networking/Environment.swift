85907082ca53a8562aa0ca93be4717f75d58d063
Jans Pavlovs <jans.pavlovs@gmail.com>
2021-01-24 15:47:13 +0200

    Add environment configuration

---
+++ Intexsys/Infrastructure/Networking/Environment.swift
@@ -0,0 +1,18 @@
+//
+//  Environment.swift
+//  Intexsys
+//
+//  Created by Jans Pavlovs on 24/01/2021.
+//
+
+import Foundation
+
+// MARK: Initialization
+
+enum Environment: String {
+    var path: String {
+        "https://\(self.rawValue)/0.5"
+    }
+
+    case development = "www.opticsplanet.com"
+}
